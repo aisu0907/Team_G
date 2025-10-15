b1536138fd2f1d09c6230a6c12ff8f264834e67d
tenjikugenki <h1202515016@hiratagakuen.onmicrosoft.com>
2025-10-15 16:32:29 +0900

    Added Reflect

--- Team_G/Assets/TenjikuGenki/Sheild.cs
+++ Team_G/Assets/TenjikuGenki/Sheild.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Sheild : MonoBehaviour
 {
@@ -9,10 +10,16 @@ public class Sheild : MonoBehaviour
     public GameObject follow;
     Vector2 tmp_v;
 
+    Vector2 moveDirection_s = new Vector2(1,1);
+    public float moveSpeed_s = 5.0f;
+
+    Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         tmp_s = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -31,6 +38,32 @@ public class Sheild : MonoBehaviour
         if (Input.GetKey(KeyCode.X))
         {
             tmp_s.sprite = GSheild;
+        }
+    }
+
+    //void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Green")
+    //    {
+    //        Debug.Log("Hit");
+    //        Destroy(collision.gameObject);
+    //    }
+    //}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Green"))
+        {
+            fall test = collision.gameObject.GetComponent<fall>();
+
+            moveSpeed_s = test.speed;
+            moveDirection_s = test.moveDirection;
+
+            // 法線ベクトルを使って反射する
+            Vector2 normal = collision.contacts[0].normal;
+            moveDirection_s = Vector2.Reflect(moveDirection_s, normal);
+
+            // 速度を更新
+            collision.rigidbody.linearVelocity = moveDirection_s.normalized * -moveSpeed_s;
         }
     }
 }
