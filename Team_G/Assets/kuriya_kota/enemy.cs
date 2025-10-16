2aaa9c454d99e7919fa564993f70a2d78a92226b
Getuyobi0926 <bskrn313@gmail.com>
2025-10-16 12:00:07 +0900

    レタス

--- Team_G/Assets/kuriya_kota/enemy.cs
+++ Team_G/Assets/kuriya_kota/enemy.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
     public Vector2 moveDirection = new Vector2(1, 1); // 初期移動方向
     public float moveSpeed = 5f;
+    float enemy_position=0.0f;
 
     private Rigidbody2D rb;
 
@@ -14,6 +16,16 @@ public class Enemy : MonoBehaviour
         rb.linearVelocity = moveDirection.normalized * moveSpeed;
     }
 
+    private void Update()
+    {
+        GetComponent<Position>();
+
+        
+
+    }
+
+
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
@@ -23,7 +35,7 @@ public class Enemy : MonoBehaviour
             moveDirection = Vector2.Reflect(moveDirection, normal);
 
             // 速度を更新
-            rb.linearVelocity = moveDirection.normalized * moveSpeed;
+            rb.linearVelocity = moveDirection.normalized * moveSpeed*2.0f;
         }
     }
 }
