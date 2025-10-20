ae11c0ee01c84ea7f7598b2e8f6c88711ea71302
Getuyobi0926 <bskrn313@gmail.com>
2025-10-20 12:26:49 +0900

    しーんつくった：）

--- Team_G/Assets/kuriya_kota/k_return.cs
+++ Team_G/Assets/kuriya_kota/k_return.cs
@@ -8,7 +8,7 @@ public class k_return : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Return))
         {
-            SceneManager.LoadScene("Play_Scene");
+            SceneManager.LoadScene("k_Play_Scene");
         }
     }
 }
