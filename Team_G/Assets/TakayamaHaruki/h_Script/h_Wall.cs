0bdeab1ee9b817146b61b32d092ac4849ec26a17
TAKAYAMAHARUKI <h1202515022@hiratagakuen.onmicrosoft.com>
2025-10-20 14:03:54 +0900

    アイテム

--- Team_G/Assets/TakayamaHaruki/h_Script/h_Wall.cs
+++ Team_G/Assets/TakayamaHaruki/h_Script/h_Wall.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class h_Wall : MonoBehaviour
+public class k_Wall : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
