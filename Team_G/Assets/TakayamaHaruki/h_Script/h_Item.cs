ac44436eac53134761df652f2bcf29c7ae7818d5
TAKAYAMAHARUKI <h1202515022@hiratagakuen.onmicrosoft.com>
2025-10-21 11:07:59 +0900

    進捗更新

--- Team_G/Assets/TakayamaHaruki/h_Script/h_Item.cs
+++ Team_G/Assets/TakayamaHaruki/h_Script/h_Item.cs
@@ -1,21 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class h_Item : MonoBehaviour
 {
+    [SerializeField] List<GameObject> itemList;
+    Vector2 v;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        Debug.Log("ƒAƒCƒeƒ€");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
+        int randitem = Random.Range(0, itemList.Count);
+        v = new Vector2(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y);
+
+        if (collision.gameObject.GetComponent<g_enemy>().OnHitting != true)
+        {
+            Instantiate(itemList[randitem], v , Quaternion.identity);
+        }
     }
 }
