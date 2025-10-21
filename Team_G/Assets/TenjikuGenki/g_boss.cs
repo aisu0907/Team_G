572d84975cf74e021a1c23267407c9896898c043
tenjikugenki <h1202515016@hiratagakuen.onmicrosoft.com>
2025-10-21 12:18:49 +0900

    aa

---
+++ Team_G/Assets/TenjikuGenki/g_boss.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class g_boss : MonoBehaviour
+{
+    public int Health = 3;  //‘Ì—Í
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
