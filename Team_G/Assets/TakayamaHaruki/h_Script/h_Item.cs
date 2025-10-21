a38971116a7dfd4d0bd80a5322691bd3b2c9854f
TAKAYAMAHARUKI <h1202515022@hiratagakuen.onmicrosoft.com>
2025-10-21 11:30:29 +0900

    レイヤー調整

--- Team_G/Assets/TakayamaHaruki/h_Script/h_Item.cs
+++ Team_G/Assets/TakayamaHaruki/h_Script/h_Item.cs
@@ -9,22 +9,22 @@ public class h_Item : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        Debug.Log("アイテム");
+     
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-        int randitem = Random.Range(0, itemList.Count);
         v = new Vector2(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y);
+        int randitem = Random.Range(0, itemList.Count);
+            Debug.Log("アイテム");
 
-        if (collision.gameObject.GetComponent<g_enemy>().OnHitting != true)
+        if (collision.gameObject.GetComponent<g_enemy>().OnHitting == false)
         {
             Instantiate(itemList[randitem], v , Quaternion.identity);
         }
