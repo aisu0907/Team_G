6d5c7a3fade0f2a2163c3b553c4a5ea359f7e710
TAKAYAMAHARUKI <h1202515022@hiratagakuen.onmicrosoft.com>
2025-10-22 12:38:56 +0900

    アイテム

--- Team_G/Assets/TakayamaHaruki/h_Script/h_Item.cs
+++ Team_G/Assets/TakayamaHaruki/h_Script/h_Item.cs
@@ -3,17 +3,46 @@ using UnityEngine;
 public class h_Item : MonoBehaviour
 {
     public Rigidbody rb;
-    public float fall_Velocity = 0.0f;
-    
+    public float fall_Velocity = 3.0f;
+    public int item_id;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-           
+      
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2 (0, fall_Velocity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //シールドに当たった場合
+        if(collision.gameObject.tag == "Sheild")
+        {
+            //アイテムを削除
+            Destroy(this);
+            //スピードアップ
+            if (item_id == 0)
+            {
+
+            }
+            else if(item_id == 1)
+            {
+                
+            }
+            else if(item_id == 2)
+            {
+
+            }
+        }
     }
 }
