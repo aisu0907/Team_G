bb3a88c4a163167c8dfabc4b5ef7fa480fb90df0
Getuyobi0926 <bskrn313@gmail.com>
2025-10-22 13:17:49 +0900

    uiを作り始めた:)

---
+++ Team_G/Assets/kuriya_kota/k_health.cs
@@ -0,0 +1,44 @@
+//HP‚Ì•\Ž¦
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class k_health : MonoBehaviour
+{
+    public Sprite HP3;
+    public Sprite HP2;
+    public Sprite HP1;
+    public Sprite HP0;
+
+    SpriteRenderer img;
+
+    public GameObject health;
+   
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        img = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int hp = health.GetComponent<Player>().Health;
+
+        switch (hp) {
+            case 3:
+                img.sprite = HP3;
+                break;
+            case 2:
+                img.sprite = HP2;
+                break;
+            case 1:
+                img.sprite = HP1;
+                break;
+            case 0:
+                img.sprite = HP0;
+                break;
+        }
+    }
+}
