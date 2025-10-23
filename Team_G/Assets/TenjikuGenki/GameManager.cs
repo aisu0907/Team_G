15b89e9fc0ed138c88a8403bc888f98b6b128242
tenjikugenki <h1202515016@hiratagakuen.onmicrosoft.com>
2025-10-23 12:34:19 +0900

    no message

--- Team_G/Assets/TenjikuGenki/GameManager.cs
+++ Team_G/Assets/TenjikuGenki/GameManager.cs
@@ -1,22 +1,23 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    public int GameTimer, Faze = 0;
-    public int Score;
-    public int[] KillCnt = { 0, 0, 0 };
+//public class GameManager : MonoBehaviour
+//{
+//    [SerializeField] private EnemySpawner enemySpawner;
+//    public int GameTimer, Faze = 0;
+//    public int Score;
+//    public int[] KillCnt = { 0, 0, 0 };
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        // フェーズ切り替え
-        Faze = 1;
-        Debug.Log(GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().Health);
-    }
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+//        // フェーズ切り替え
+//        Faze = 1;
+//        Debug.Log(GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().Health);
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
 
-    }
-}
+//    }
+//}
--- Team_G/Assets/TenjikuGenki/g_boss.cs
+++ Team_G/Assets/TenjikuGenki/g_boss.cs
@@ -1,28 +1,56 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class g_boss : CharacterBase
 {
+    int timer;
+    int mode = 0;
+    bool down = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // CharacterBaseŒp³
-        Health = 3;     //‘Ì—Í
+        Health = 1;     //‘Ì—Í
         Speed = 3.0f;   //ˆÚ“®‘¬“x
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //if (mode == 0)
+        //{
+        //    ++timer;
+        //    if (timer >= 600) mode = 1;
+        //}
+        //else if (mode == 1)
+        //{
+        //    if(down)
+        //    {
+        //        Vector2 v = new Vector2(transform.position.x, transform.position.y - 30);
+        //        transform.position = v;
+        //        if(transform.position.y <= 100) down = false;
+        //    }
+        //    else
+        //    {
+        //        Vector2 v = new Vector2(transform.position.x, transform.position.y + 30);
+        //        transform.position = v;
+        //        if (transform.position.y >= 100) down = false;
+        //    }
+        //}
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<g_enemy>().OnHitting)
         {
             --Health;
-            Debug.Log("gue-----------");
             Destroy(collision.gameObject);
-            if (Health == 0) Destroy(gameObject);
+            if (Health == 0)
+            {
+                Destroy(gameObject);
+                SceneManager.LoadScene("Result_Scene");
+            }
         }
     }
 }
