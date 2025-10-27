aa8bddba4c51550ea3d4acd4e28329fa79507fb3
TAKAYAMAHARUKI <h1202515022@hiratagakuen.onmicrosoft.com>
2025-10-27 12:00:00 +0900

    ボス戦追加

--- Team_G/Assets/kuriya_kota/k_boss_wall.cs
+++ Team_G/Assets/kuriya_kota/k_boss_wall.cs
@@ -3,21 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class k_boss_wall : MonoBehaviour
 {
-   public int boss_health = 5;
+    public int boss_health = 5;
     public string nextSceneName;
     public GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private void Update()
     {
-        if (player.GetComponent<Player>().Health <= 0)
-        {
-            SceneManager.LoadScene("Gameover_Scene");
-        }
-        if (player.GetComponent<Player>().Health <= 0)
-        {
-            Debug.Log("shinu");
-        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -25,17 +17,6 @@ public class k_boss_wall : MonoBehaviour
         if (collision.gameObject.tag == "Enemy")
         {
             Destroy(collision.gameObject);
-            boss_health--;
-            Debug.Log("Boss health:"+boss_health);
-
-            if(boss_health <= 0)
-            {
-                SceneManager.LoadScene("Result_Scene");
-            }
-
-           
-
-
         }
 
     }
