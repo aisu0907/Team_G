c8891a86a569f17bb0a8336d4b32f40ac40a1871
TAKAYAMAHARUKI <h1202515022@hiratagakuen.onmicrosoft.com>
2025-11-05 16:29:36 +0900

    メインシーン　ボムゲージオブジェクト追加

--- Team_G/Assets/TakayamaHaruki/h_Script/h_Bomb_Gage.cs
+++ Team_G/Assets/TakayamaHaruki/h_Script/h_Bomb_Gage.cs
@@ -1,33 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class h_Bomb_Gage : MonoBehaviour
 {
     public float bomb_gage_up = 0; //時間経過で進むボムゲージ
     public int bomb_time = 0;    //ボムゲージが進む頻度
+    public Slider bomb_gage;
 
     private int frame = 0;
-    private Vector2 v = new Vector2(0, 30);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.transform.localScale = v;
+        bomb_gage.value = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.localScale = v;
-
         frame++;
+
         if(frame == bomb_time)
         {
             frame = 0;
-            v.x = bomb_gage_up;
+            bomb_gage.value = bomb_gage_up;
         }
 
-        if (v.x > 200)
+        if (bomb_gage.value > 200)
         {
-            v.x = 0;
+            bomb_gage.value = 0;
         }
         
     }
