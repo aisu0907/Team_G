d667667c72dfa77f0b112674c1e5b1cd0e6020e1
TAKAYAMAHARUKI <h1202515022@hiratagakuen.onmicrosoft.com>
2025-11-06 11:17:19 +0900

    FadeManager追加

--- Team_G/Assets/TakayamaHaruki/h_Script/h_Bomb_Gage.cs
+++ Team_G/Assets/TakayamaHaruki/h_Script/h_Bomb_Gage.cs
@@ -8,15 +8,15 @@ public class h_Bomb_Gage : MonoBehaviour
     public float bomb_gage_up = 0; //時間経過で進むボムゲージ
     public float bomb_max = 0;     //ボム最大所持数
     public int bomb_time = 0;      //ボムゲージが進む頻度
-    public Slider bomb_gage;
+    public Slider bomb_gage;       //スライダーを取得
 
     private int frame = 0;
-    private int max_bom = 2;
+    private int max_bom = 2;//ボムの最大所持数
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        bomb_gage.value = 0;
+        bomb_gage.value = 0;　//スライダーの位置をリセット
     }
 
     // Update is called once per frame
@@ -24,16 +24,23 @@ public class h_Bomb_Gage : MonoBehaviour
     {
         frame++;
 
+        //1秒間にためるゲージ
         if(frame >= bomb_time)
         {
+            //frameをリセット
             frame = 0;
+            //ゲージを増やす
             bomb_gage.value += bomb_gage_up;
         }
 
+        //ゲージがmaxの場合
         if (bomb_gage.value >= bomb_max)
         {
+            //ゲージをリセット
             bomb_gage.value = 0;
+            //プレイヤーのボム所持数が最大じゃない場合
             if (Player.Instance.bom < max_bom)
+                //ボムを1個増やす
                 Player.Instance.bom++;
         }
         
