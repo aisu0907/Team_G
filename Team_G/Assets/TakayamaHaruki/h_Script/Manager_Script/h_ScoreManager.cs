6eb5f8281d0b4fb53e5cd28e9cb0f6f2ce2333ac
TAKAYAMAHARUKI <h1202515022@hiratagakuen.onmicrosoft.com>
2025-11-11 12:22:58 +0900

    画面外に行った時にもスコア適用

--- Team_G/Assets/TakayamaHaruki/h_Script/Manager_Script/h_ScoreManager.cs
+++ Team_G/Assets/TakayamaHaruki/h_Script/Manager_Script/h_ScoreManager.cs
@@ -6,10 +6,10 @@ using System.Collections.Generic;
 
 public class Score_Manager : MonoBehaviour
 {
-    public float score_rate = 0;
-    public int item_score = 0;
+    public float score_rate = 0; //エネミー同士がぶつかったときのスコア倍率
+    public int item_score = 0;   //アイテムスコア
 
-    private float enemy_score = 0;
+    private float enemy_score = 0; //エネミースコアを保存用
 
     public static Score_Manager Instance { get; private set; }
 
@@ -29,20 +29,30 @@ public class Score_Manager : MonoBehaviour
 
     }
 
+    //エネミー衝突スコア関数
     public void OnEnemiesCollided(Enemy e1, Enemy e2)
     {
+        //ぶつかったエネミー同士のIDを取得してペアを作る
         var key = (e1.GetInstanceID() ^ e2.GetInstanceID()).ToString();
+        //ぶつかったエネミー同士のペアが存在しない場合
         if(!recentCollisions.Contains(key))
         {
-            recentCollisions.Add(key);
-            enemy_score = (float)((e1.score + e2.score) * score_rate);
-            Score.Instance.total_score += (int)enemy_score;
+            recentCollisions.Add(key); //ペアを追加
+            enemy_score = (float)((e1.score + e2.score) * score_rate); //スコア倍率を乗せる
+            Score.Instance.total_score += (int)enemy_score; //スコアを追加
         }
     }
 
+    //アイテムスコア関数
     public void ItemScore()
     {
-        Score.Instance.total_score += item_score;
+        Score.Instance.total_score += item_score; //スコア追加
+    }
+
+    //エネミースコア関数
+    public void Enemy_Score(Enemy e)
+    {
+        Score.Instance.total_score += e.score; //スコア追加
     }
 
     private HashSet<string> recentCollisions = new HashSet<string>();
