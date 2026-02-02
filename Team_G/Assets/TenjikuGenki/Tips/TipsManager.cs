4fb15f896f47f75a7e95be814207c9cddb0e99f4
tenjikugenki <h1202515016@hiratagakuen.onmicrosoft.com>
2026-02-02 16:15:27 +0900

    Tips終生

--- Team_G/Assets/TenjikuGenki/Tips/TipsManager.cs
+++ Team_G/Assets/TenjikuGenki/Tips/TipsManager.cs
@@ -28,6 +28,19 @@ public class TipsManager : MonoBehaviour
 
         if (Player.Instance.health > 0)
         {
+            if(go.TryGetComponent<GameManager>(out var gm))
+            {
+                if ((pm.phase + 1) % 2 == 0)
+                {
+                    tips.color = new Color(255, 0, 0);
+                }
+                else
+                {
+                    tips.color = new Color(255, 255, 255);
+                }
+
+            }
+
             timer++;
             if (pm.phase == 8)
             {
@@ -37,7 +50,7 @@ public class TipsManager : MonoBehaviour
             {
                 if (tips.text.Length == 2)
                 {
-                    ChangeNextTips(pm.phase + 1);
+                    //ChangeNextTips(pm.phase + 1);
                 }
                 else
                 {
@@ -46,7 +59,22 @@ public class TipsManager : MonoBehaviour
                     timer = 0;
                     ch = !ch;
                 }
+            }
 
+            if(go.TryGetComponent<GameManager>(out var game_m))
+            {
+                if (gm.phase == 0)
+                {
+                    if (timer == 300)
+                    {
+                        if (ch) ChangeNextTips(0);
+                        if (!ch) ChangeNextTips(1);
+                        timer = 0;
+                        ch = !ch;
+                    }
+                }
+                else
+                    ChangeNextTips(pm.phase + 1);
             }
         }
     }
