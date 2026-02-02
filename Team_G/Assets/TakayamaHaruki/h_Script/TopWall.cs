95c15d24e344742dbefdb741cdd5b65b4b7e4616
TAKAYAMAHARUKI <h1202515022@hiratagakuen.onmicrosoft.com>
2026-02-02 12:23:14 +0900

    コメント追加 スクリプト整理

--- Team_G/Assets/TakayamaHaruki/h_Script/TopWall.cs
+++ Team_G/Assets/TakayamaHaruki/h_Script/TopWall.cs
@@ -2,36 +2,23 @@
 
 using UnityEngine;
 
-public class TopWall : MonoBehaviour, IPhazeManager
+public class TopWall : MonoBehaviour
 {
-    public GameObject manger;
-    public int phase { get; set; } = 0;
-    public bool is_change_color { get; set; } = false;
-    private IPhazeManager ipm;
-
-    private void Start()
-    {
-       ipm = manger.GetComponent<IPhazeManager>();
-    }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(ipm.phase >= 6 && collision.gameObject.CompareTag("Enemy"))
+        //壁に触れたらエネミーを削除
+        if (collision.gameObject.CompareTag("Enemy"))
         {
-            manger.GetComponent<TutorialManager>().enemy_hit_count++;
-            //エネミーを削除
-            Destroy(collision.gameObject);
-            return;
-        }
-            //壁に触れたらエネミーを削除
-            if (collision.gameObject.CompareTag("Enemy"))
-        {
-            //触れたエネミーの情報を保存
-            Enemy other = collision.gameObject.GetComponent<Enemy>();
-            
-            if (other != null)
+            if (!collision.gameObject.GetComponent<TutorialEnemy>())
             {
-                //壁に触れた敵のスコアを追加
-                ScoreManager.Instance.Enemy_Score(other);
+                //触れたエネミーの情報を保存
+                Enemy other = collision.gameObject.GetComponent<Enemy>();
+
+                if (other != null)
+                {
+                    //壁に触れた敵のスコアを追加
+                    ScoreManager.Instance.Enemy_Score(other);
+                }
             }
             //エネミーを削除
             Destroy(collision.gameObject);
